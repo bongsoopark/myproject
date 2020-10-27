7465d3ccaaeb4e82f288657af440e1d14dbda7a9
Bongsoo Park <bongsoopark@Bongsoos-MacBook-Pro.local>
2020-10-27 09:49:27 -0400

    updated

--- test_barcode.py
+++ test_barcode.py
@@ -1,9 +1,26 @@
-#TEST
-barcode = "ATGCATGC"
-seq = "ATGCATGCAAAACCC"
+# 10/26/2020
+# Test barcode
+import re # Regular expression 
 
-import re
-match = re.sub(barcode,"",seq)
-
-print(seq)
-print(match)
+f = open("./test_files/SRR11972514_1.fastq")
+barcode = "GGAAAT"
+cnt = 1
+print(barcode)
+for line in f:
+	line = line.strip()
+	if cnt == 1:
+		# Sequence id
+		pass
+	if cnt == 2:
+		# Sequence reads
+		print(line)
+		match = re.sub(barcode,"barcode!!!",line)
+		print(match)
+	if cnt == 3:
+		# + sign
+		pass
+	if cnt == 4:
+		# Sequence quality
+		cnt = 0
+	cnt += 1
+f.close()
